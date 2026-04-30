1e90b0f7c7321b2fecc657aeecad7b10b5abb75a
Nakhid18 <Nakhid.Ibrahimov@lnu.edu.ua>
2026-04-30 11:32:23 +0300

    Add Money struct and related function declarations

---
+++ oop1.h
@@ -0,0 +1,16 @@
+#ifndef MONEY_H
+#define MONEY_H
+
+struct Money
+{
+    int grn;
+    short int cop;
+};
+
+void addMoney(Money& money, Money& addMoney),
+multiplyMoney(Money& money, int count),
+roundMoney(Money& money),
+printMoney(Money& money),
+calcGeneralPrice(const char* path);
+
+#endif
